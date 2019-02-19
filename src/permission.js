f0e29b24e868b8e179cea91ffa1d303aa631bc9d
huxuwei <hu_xuwei@163.com>
2019-02-19 23:09:50 +0800

    load

--- src/permission.js
+++ src/permission.js
@@ -5,20 +5,27 @@ import store from "./store"
 router.beforeEach((to, from, next) => {
   //to and from are Route Object,next() must be called to resolve the hook}
   console.log(arguments)
-  // 
+  // 白名单
   let whiteList = ['login']
-  if(localStorage.getItem('TOKEN')){
-    // console.log(1)
+  //  
+  if(localStorage.getItem('TOKEN'))
+  {
+    // 已有token
     if(to.path === '/login'){
       next('/')
+    }else{
+      // if(){
+
+      // }else{
+        
+      // }
     }
   }else{
+    // 没有token
     if(whiteList.includes(to.path)){
       next()
     }else{
       next('/login')
     }
   }
-  console.log(to)
-  // next()
 })
